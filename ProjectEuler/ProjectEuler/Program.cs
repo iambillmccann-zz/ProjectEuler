5085ed30f1550df7bf5ae05f465e24f4d4fb5d79
iambillmccann <bill.mccann@gmail.com>
2020-01-23 18:39:02 -0500

    Add simple validation
    Validate the user input.

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -9,48 +9,79 @@ namespace ProjectEuler.Problems
     {
         private static Stopwatch totalTime;
         private static string quitString = "Q";
+        private static int lastProblem = 51;
 
-        private static string getUserInput()
+        /// <summary>
+        /// getUserInput is a simple method for reading from the console.
+        /// </summary>
+        /// <returns>A string that represents the user's input.</returns>
+        private static int getUserInput()
         {
             Console.Write("What problem shall I run? Or type 'Q' to quit. ");
-            return Console.ReadLine();
+            return checkUserInput(Console.ReadLine());
+        }
+
+        /// <summary>
+        /// checkUserInput will validate the user's input. This is a very redimentary
+        /// validation method. It checks three things ...
+        /// 1. Checks for the letter "Q"
+        /// 2. Checks for an integer
+        /// 3. Checks that the integer is positive
+        /// 4. Checks that the integer is no greater than the last problem solved.
+        /// These checks assume that problems are solved in order.
+        /// Note: If the user enters "Q", a minus one is returned to the caller.
+        /// </summary>
+        /// <param name="userInput">A string entered by the user.</param>
+        /// <returns>A valid integer result.</returns>
+        private static int checkUserInput(string userInput)
+        {
+            int result = -1;
+            if (userInput.ToUpper() == quitString) return result;
+
+            try
+            {
+                result = Convert.ToInt32(userInput);
+                if (result < 1)
+                {
+                    Console.WriteLine("\nBTW, problem numbers are positive integers.");
+                    result = getUserInput();
+                } else if (result > lastProblem)
+                {
+                    Console.WriteLine("\nI have only completed problems 1 through " + lastProblem.ToString());
+                    result = getUserInput();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nSorry but I did not understand that. Please type the problem number or Q to quit.");
+                result = getUserInput();
+            }
+
+            return result;
         }
 
         static void Main(string[] args)
         {
             int problemNumber = 0;
-            string userInput = null;
 
             ProblemFactory problemFactory = new ProblemFactory();
             totalTime = new Stopwatch();
 
-            userInput = getUserInput();
-            
-            while (userInput.ToUpper() != quitString)
+            problemNumber = getUserInput();
+
+            while (problemNumber > 0)
             {
-                try
-                {
-                    problemNumber = Convert.ToInt32(userInput);
-                    //ToDo check the problem number to verify that it has been implemented.
+                totalTime.Reset();
+                totalTime.Start();
+                string result = problemFactory.GetSolution(problemNumber).Compute();
+                totalTime.Stop();
 
-                    totalTime.Reset();
-                    totalTime.Start();
-                    string result = problemFactory.GetSolution(problemNumber).Compute();
-                    totalTime.Stop();
-
-                    Console.WriteLine("\n-----------------------------------------------------------------------");
-                    Console.WriteLine("Solution to problem " + problemNumber + " = " + result);
-                    Console.WriteLine("Execution time was " + Utilities.FormatMilliseconds(totalTime.ElapsedMilliseconds));
-                    Console.WriteLine("-----------------------------------------------------------------------");
-                    Console.WriteLine();
-
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("\nSorry but I did not understand that. Please type the problem number or Q to quit.");
-                }
-
-                userInput = getUserInput();
+                Console.WriteLine("\n-----------------------------------------------------------------------");
+                Console.WriteLine("Solution to problem " + problemNumber + " = " + result);
+                Console.WriteLine("Execution time was " + Utilities.FormatMilliseconds(totalTime.ElapsedMilliseconds));
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine();
+                problemNumber = getUserInput();
             }
 
             Console.WriteLine("Goodbye. Press any key to continue");
