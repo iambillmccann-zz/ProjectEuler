3c8bb30a07a3ba3cd3f589134cf6ef48bc617bef
iambillmccann <bill.mccann@gmail.com>
2020-03-25 14:17:52 -0400

    Add unit tests

--- ProjectEuler/ProjectEulerTests/MathLibrary.cs
+++ ProjectEuler/ProjectEulerTests/MathLibrary.cs
@@ -45,7 +45,35 @@ namespace UtilitiesTests
             Assert.AreEqual(false, MathLibrary.IsMultiple(13, 3));
             Assert.AreEqual(false, MathLibrary.IsMultiple(19, 18));
             Assert.AreEqual(false, MathLibrary.IsMultiple(23, 2));
-            Assert.AreEqual(false, MathLibrary.IsMultiple(37, 6));
+            Assert.AreEqual(false, MathLibrary.IsMultiple(37, 66));
+        }
+    }
+
+    [TestClass]
+    public class SimpleMath
+    {
+        [TestMethod]
+        public void Square()
+        {
+            Assert.AreEqual(0, MathLibrary.Square(0));
+            Assert.AreEqual(1, MathLibrary.Square(-1));
+            Assert.AreEqual(25, MathLibrary.Square(5));
+        }
+
+        [TestMethod]
+        public void NaturalSum()
+        {
+            Assert.AreEqual(55, MathLibrary.SumNatural(10));
+            Assert.AreEqual(0, MathLibrary.SumNatural(0));
+            Assert.AreEqual(0, MathLibrary.SumNatural(-10));
+        }
+
+        [TestMethod]
+        public void NaturalSumSquare()
+        {
+            Assert.AreEqual(385, MathLibrary.SumNaturalSquares(10));
+            Assert.AreEqual(0, MathLibrary.SumNaturalSquares(0));
+            Assert.AreEqual(0, MathLibrary.SumNaturalSquares(-10));
         }
     }
 }
